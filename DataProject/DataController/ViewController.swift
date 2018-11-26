c59e7ce6bf23e7625412c55376ad178a16cdaa0c
Spencer B <spencerbillings2000@gmail.com>
2018-11-26 09:56:27 -0700

    Created the project, organized/refound files, and fixed method headers

---
+++ DataProject/DataController/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DataProject
+//
+//  Created by Billings, Spencer on 11/26/18.
+//  Copyright © 2018 Billings, Spencer. All rights reserved.
+//
+
+import UIKit
+
+public class ViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    public override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
