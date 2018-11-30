ba2f0b4e97994e72b6fa81f726211ea783411597
Spencer B <spencerbillings2000@gmail.com>
2018-11-30 09:11:34 -0700

    Added author, text, and symbol labels to the storyboard and updated BucketitemLIst

--- DataProject/Controller/BucketItemCell.swift
+++ DataProject/Controller/BucketItemCell.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class BucketItemCell: UITableViewCell {
+class BucketItemCell: UITableViewCell
+{
+    var currentBucketItem : BucketItem!
+    {
+        updateCellView()
+    }
 
-    override func awakeFromNib() {
+    public override func awakeFromNib() -> Void
+    {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    public override func setSelected(_ selected: Bool, animated: Bool) -> Void
+    {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+    @IBOutlet weak var bucketItemText : UILabel!
+    @IBOutlet weak var bucketItemSymbol : UILabel!
+    @IBOutlet weak var bucketItemSignature : UILabel!
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+//      let symbolStart = 0x1F680
+//      let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+//      let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
 }
